05d4fb7021d710707131a0a697d35001d6f7e334
jaywon joo <jaywonjoo@gmail.com>
2025-07-19 21:22:49 -0400

    Made a calculator!

---
+++ calculator.py
@@ -0,0 +1,15 @@
+val1 = input("Please enter your first number:")
+val2 = input("Please enter your second number:")
+val3 = input("what would operation would you like to perform?:")
+
+x = int(val1)
+y = int(val2)
+operation = val3
+
+if val3 == "+" or val3 == "addition":
+    result = x + y
+    print(f"the result is: {result}")
+else:
+    print("Sorry, I don't know how to do anything but add :(")
+
+# Need to add other operations
