06f49abe4c0c393294158396bc3f6c3bd1fff57d
jaywonjoo <110232949+jaywonjoo@users.noreply.github.com>
2025-07-21 13:22:06 +0000

    Created v1 of rock papers scissors project!

---
+++ rockpaperscissors.py
@@ -0,0 +1,8 @@
+userchoice = input("please enter rock, paper, or scissors!")
+
+result = "rock"
+
+if userchoice == "paper":
+    print(f"you win! the computer chose {result}")
+else:
+    print(f"I'm not set up to tackle any other situations yet :(")
