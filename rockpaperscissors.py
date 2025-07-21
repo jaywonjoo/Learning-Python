bf9e510dd1723e133ae7ea64a7f2c23b0abd39b2
jaywonjoo <110232949+jaywonjoo@users.noreply.github.com>
2025-07-21 14:32:23 +0000

    Added other outcomes to rock/paper/scissors game

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-userchoice = input("please enter rock/paper/scissors")
-=======
 userchoice = input("please enter rock, paper, or scissors!")
->>>>>>> 06f49abe4c0c393294158396bc3f6c3bd1fff57d
 
-<<<<<<< HEAD
-print(f"you win! the computer chose {result}")
-=======
-result = "rock"
+computerchoice = "rock"
 
-if userchoice == "paper":
-    print(f"you win! the computer chose {result}")
+if userchoice == "rock":
+    print(f"you tie! the computer chose {computerchoice}")
+elif userchoice == "paper":
+    print(f"you win! the computer chose {computerchoice}")
+elif userchoice == "scissors":
+    print(f"you lose! the computer chose {computerchoice}")
 else:
-    print(f"I'm not set up to tackle any other situations yet :(")
->>>>>>> 06f49abe4c0c393294158396bc3f6c3bd1fff57d
+    print(f"Looks like you chose an unspecified option! Please try typing in your choice again")
