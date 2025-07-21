f24e7aefa5e3d90239e196d704f96bc6d14efe74
jaywonjoo <110232949+jaywonjoo@users.noreply.github.com>
2025-07-21 14:37:13 +0000

    Implemented .lower() in the rock-paper-scissors game to make user input case-insensitive.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,12 +1,16 @@
 userchoice = input("please enter rock, paper, or scissors!")
 
+userchoicelower = userchoice.lower()
+
 computerchoice = "rock"
 
-if userchoice == "rock":
+if userchoicelower == "rock":
     print(f"you tie! the computer chose {computerchoice}")
-elif userchoice == "paper":
+elif userchoicelower == "paper":
     print(f"you win! the computer chose {computerchoice}")
-elif userchoice == "scissors":
+elif userchoicelower == "scissors":
     print(f"you lose! the computer chose {computerchoice}")
 else:
     print(f"Looks like you chose an unspecified option! Please try typing in your choice again")
+
+#originally had multiple if statements but it was causing the else to print. elif makes it so that the else statement isn't tied to the last option and only runs if all the others aren't true
