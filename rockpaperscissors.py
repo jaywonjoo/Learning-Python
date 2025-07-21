ec0afe9f223a4e9f026f1fa59286220a25cec6d3
jaywonjoo <110232949+jaywonjoo@users.noreply.github.com>
2025-07-21 17:54:42 +0000

    RPS: Computer now chooses it's own option as well!

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -17,7 +17,28 @@
 
 #originally had multiple if statements but it was causing the else to print. elif makes it so that the else statement isn't tied to the last option and only runs if all the others aren't true
 
+
+
+#user chooses their move
+userchoice = input("please enter rock, paper, or scissors!")
+userchoicelower = userchoice.lower()
+
+#computer chooses their move
 import random
 computerchoices = ["rock","paper","scissors"]
 randomcomputerchoice = random.choice(computerchoices)
-print(randomcomputerchoice)
+
+if userchoicelower not in computerchoices:
+    print("Sorry, invalid choice! Please reenter your choice!")
+else:
+    #computer vs user to decide winner
+    if userchoicelower == randomcomputerchoice:
+        print(f"you tie! the computer chose {randomcomputerchoice}")
+    elif (
+        (userchoicelower == "paper" and randomcomputerchoice == "rock") or
+        (userchoicelower == "rock" and randomcomputerchoice == "scissors") or
+        (userchoicelower == "scissors" and randomcomputerchoice == "paper")
+    ):
+        print(f"you win! the computer chose {randomcomputerchoice}")
+    else:
+        print(f"you lose! the computer chose {randomcomputerchoice}")
