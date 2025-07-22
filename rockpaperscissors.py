aed68f53692eac89471063ce701e80323ede8e45
jaywonjoo <110232949+jaywonjoo@users.noreply.github.com>
2025-07-22 13:18:30 +0000

    Added comments for organization

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,24 +1,18 @@
+#Take 1
+    # userchoice = input("please enter rock, paper, or scissors!")
+    # userchoicelower = userchoice.lower()
+    # computerchoice = "rock"
+    # if userchoicelower == "rock":
+    #     print(f"you tie! the computer chose {computerchoice}")
+    # elif userchoicelower == "paper":
+    #     print(f"you win! the computer chose {computerchoice}")
+    # elif userchoicelower == "scissors":
+    #     print(f"you lose! the computer chose {computerchoice}")
+    # else:
+    #     print(f"Looks like you chose an unspecified option! Please try typing in your choice again")
+    #originally had multiple if statements but it was causing the else to print. elif makes it so that the else statement isn't tied to the last option and only runs if all the others aren't true
 
-
-# userchoice = input("please enter rock, paper, or scissors!")
-# userchoicelower = userchoice.lower()
-
-
-# computerchoice = "rock"
-
-# if userchoicelower == "rock":
-#     print(f"you tie! the computer chose {computerchoice}")
-# elif userchoicelower == "paper":
-#     print(f"you win! the computer chose {computerchoice}")
-# elif userchoicelower == "scissors":
-#     print(f"you lose! the computer chose {computerchoice}")
-# else:
-#     print(f"Looks like you chose an unspecified option! Please try typing in your choice again")
-
-#originally had multiple if statements but it was causing the else to print. elif makes it so that the else statement isn't tied to the last option and only runs if all the others aren't true
-
-
-
+#Take 2
 #user chooses their move
 userchoice = input("please enter rock, paper, or scissors!")
 userchoicelower = userchoice.lower()
