0d0d69378e69a89b0042f350807d5dd93c3ccfd4
jaywonjoo <110232949+jaywonjoo@users.noreply.github.com>
2025-07-22 13:17:06 +0000

    Added comments to understand functions

--- countdowntimer.py
+++ countdowntimer.py
@@ -3,6 +3,10 @@ import time
 user_seconds = input("Please enter your desired seconds:")
 user_seconds_int = int(user_seconds)
 
+#Set boundary for user input
+# end='\r' rewinds the cursor to the start of the current line so that the next output overwrites it.
+# time.sleep(1) pauses the program for 1 second.
+# user_seconds_int -=1 changes the output for the viewer so they can follow along with the countdown
 if user_seconds_int > 60 or user_seconds_int < 0:
     print("Invalid input. Seconds should be between 0 and 60.")
 else:
